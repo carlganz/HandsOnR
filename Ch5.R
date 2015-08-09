3c0c9290524ac253b7e40c87409cd0797564f72c
carlganz <carl.ganz@colorado.edu>
2015-08-09 11:38:52 -0700

    Finished Ch5

---
+++ Ch5.R
@@ -0,0 +1,18 @@
+sum(deck2$face=="ace")
+
+deck2$value[deck2$face=="ace"]<-14
+
+deck4$value[deck4$suit=="hearts"]<-1
+w<-c(-1,0,1)
+x<-c(5,15)
+y<-"February"
+z<-c("Monday","Tuesday","Friday")
+w>0
+
+x>10 & x<20
+
+y=="February"
+
+all(z %in% c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))
+
+deck5$value[deck5$face=="ace"]<-NA
